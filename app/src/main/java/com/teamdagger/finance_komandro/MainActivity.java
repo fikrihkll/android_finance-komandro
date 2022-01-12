5e5aea95b8364ae372d1c126f6d5ffb1d3db3245
zackmolt <fkrihkl@gmail.com>
2022-01-12 15:04:09 +0700

    add save and use feature

--- app/src/main/java/com/teamdagger/finance_komandro/MainActivity.java
+++ app/src/main/java/com/teamdagger/finance_komandro/MainActivity.java
@@ -1,8 +1,11 @@
 package com.teamdagger.finance_komandro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -50,7 +53,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ambilDataSaldo();
 
+        binding.swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Code yang ada di dalam sini akan di panggil ketika layar di swipe
+                ambilDataSaldo();
+                binding.swipeRefresh.setRefreshing(false);
+            }
+        });
+
+    }
+
+    void ambilDataSaldo(){
+        SharedPreferences database = getSharedPreferences("database_uang", Context.MODE_PRIVATE);
+
+        int saldo = database.getInt("SALDO",0);
+
+        binding.tvTotal.setText("Rp." + saldo);
     }
 
 }
--- app/src/main/java/com/teamdagger/finance_komandro/SaveUseActivity.java
+++ app/src/main/java/com/teamdagger/finance_komandro/SaveUseActivity.java
@@ -2,8 +2,12 @@ package com.teamdagger.finance_komandro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.teamdagger.finance_komandro.databinding.ActivitySaveUseBinding;
 
@@ -20,5 +24,53 @@ public class SaveUseActivity extends AppCompatActivity {
         Intent intentReceived = getIntent();
         Boolean apakahMenyimpan = intentReceived.getBooleanExtra("KEY_SAVE_USE", false);
 
+        binding.btnSaveNominal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Code di dalam sini akan di jalankan ketika tombol di klik
+
+                if( binding.etNominal.getText().toString().isEmpty() ){
+                    Toast.makeText(getApplicationContext(), "Tolong isi data", Toast.LENGTH_LONG).show();
+                }else{
+
+                    if(apakahMenyimpan == true){
+                        String nominalStr = binding.etNominal.getText().toString();
+                        int nominal = Integer.parseInt(nominalStr);
+
+                        simpanUang(nominal);
+                    }else{
+                        String nominalStr = binding.etNominal.getText().toString();
+                        int nominal = Integer.parseInt(nominalStr);
+
+                        gunakanUang(nominal);
+                    }
+
+                }
+
+            }
+        });
     }
+    void simpanUang(int nominal){
+        SharedPreferences database = getSharedPreferences("database_uang", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editorDatabase = database.edit();
+
+        int nominalTersimpan = database.getInt("SALDO", 0);
+        nominalTersimpan = nominalTersimpan + nominal;
+
+        editorDatabase.putInt("SALDO", nominalTersimpan);
+
+        editorDatabase.apply();
+    }
+    void gunakanUang(int nominal){
+        SharedPreferences database = getSharedPreferences("database_uang", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editorDatabase = database.edit();
+
+        int nominalTersimpan = database.getInt("SALDO", 0);
+        nominalTersimpan = nominalTersimpan - nominal;
+
+        editorDatabase.putInt("SALDO", nominalTersimpan);
+
+        editorDatabase.apply();
+    }
+
 }
